616a05897c7a3b4bb9eae332e764892c8d63c205
afsar-uddin <afsarwebdev@gmail.com>
2021-10-18 08:02:08 +0600

    navbar responsive

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Link, NavLink } from 'react-router-dom';
+import Logo from '../../images/logo.png';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" variant="dark" className="ctg-navbar">
+                <Container>
+                    <NavLink to="/"><img src={Logo} alt="Logo" /></NavLink>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ctg-nav">
+                            <NavLink exact activeClassName="active" to="/">Home</NavLink>
+                            <NavLink exact activeClassName="active" to="/services">Services</NavLink>
+                            <NavLink exact activeClassName="active" to="/Contact">Contact</NavLink>
+                            <NavLink exact activeClassName="active" to="/login-register">Login / Register</NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
