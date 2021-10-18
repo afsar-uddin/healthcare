12d8e8083210ad3d5070f8889c6ea68fd15b9121
afsar-uddin <afsarwebdev@gmail.com>
2021-10-18 10:23:18 +0600

    services home short description

--- src/Components/Services/ServicesHomeItem.js
+++ src/Components/Services/ServicesHomeItem.js
@@ -3,13 +3,16 @@ import { Card, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const ServicesHomeItem = (props) => {
-    const { title, cover, id } = props.serviceHome;
+    const { title, cover, shortDsc, id } = props.serviceHome;
     return (
         <Col>
             <Card>
                 <div className="single-service">
                     <img src={cover} alt={title} />
-                    <h3>{title}</h3>
+                    <div>
+                        <h3>{title}</h3>
+                        <p>{shortDsc}</p>
+                    </div>
                 </div>
                 <Link to={`/service/${id}`}>View more</Link>
             </Card>
